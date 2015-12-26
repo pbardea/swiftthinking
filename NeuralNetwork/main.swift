eeb72f60c1978c5e84d44737a6973c66717467c4
Paul Bardea <paul@pbardea.com>
2015-12-25 23:59:52 -0500

    Initial Commit

---
+++ NeuralNetwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NeuralNetwork
+//
+//  Created by Paul Bardea on 2015-12-25.
+//  Copyright © 2015 pbardea stdios. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
