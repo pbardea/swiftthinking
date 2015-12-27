02d95af87c62d93a5196d7a83a867e9769d6bcf0
Paul Bardea <paul@pbardea.com>
2015-12-27 01:04:17 -0500

    Changed faulty test name

--- NNTests/NNTests.swift
+++ NNTests/NNTests.swift
@@ -28,7 +28,7 @@ class LinAlgTests: XCTestCase {
 
 class NNTests: XCTestCase {
     
-    func testTwoLayer() {
+    func testThreeLayer() {
         let inputSize = 3;
         
         let layer1width = 5;
