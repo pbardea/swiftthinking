10528382f649a3ed9b5392e83c481c1a86d54f62
Paul Bardea <paul@pbardea.com>
2016-11-10 07:38:00 -0800

    Initial Commit

---
+++ SwiftThinking/SwiftThinking.h
@@ -0,0 +1,19 @@
+//
+//  SwiftThinking.h
+//  SwiftThinking
+//
+//  Created by Paul Bardea on 2016-11-10.
+//  Copyright © 2016 pbardea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftThinking.
+FOUNDATION_EXPORT double SwiftThinkingVersionNumber;
+
+//! Project version string for SwiftThinking.
+FOUNDATION_EXPORT const unsigned char SwiftThinkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftThinking/PublicHeader.h>
+
+
